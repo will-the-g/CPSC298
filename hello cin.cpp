dbfd5810791668c740e754dbeb44fcf3685bcd8b
will-the-g <wgatlin@chapman.edu>
2023-01-07 16:32:23 -0800

    intial commit

---
+++ hello cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string strMessage;
+    cout << "Please type a single word message" << endl;
+    cin >> strMessage;
+    cout << "message: " << strMessage << endl;
+    return 0;
+}
